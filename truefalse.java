be4fe10151d27908ceb63557c94e6a9e5695a95e
Roman Hera <romahera6@gmail.com>
2022-07-11 19:28:15 +0300

    Add files via upload

---
+++ truefalse.java
@@ -0,0 +1,14 @@
+
+public class truefalse {
+	public static void main(String[] args) {
+	
+	int var1;
+	
+	var1 = 99;
+	if (var1 < 100) System.out.println("true");
+	if (var1 < 100) System.out.println("False");
+	if (var1 > 100) System.out.println("False out");
+	
+	}
+
+}
